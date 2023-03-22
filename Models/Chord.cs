7be42768579403727aa43237ea0b2f120735f80a
Eran Rotem <eranrotem123@gmail.com>
2023-03-22 15:45:33 -0500

    ChordSearch

--- Models/Chord.cs
+++ Models/Chord.cs
@@ -2,14 +2,18 @@
 {
     public class Chord
     {
+        //public Chord() 
+        //{
+        //    Name = Tone + Type + Extension;
+        //}
         public bool SimpleSearch { get; set; } = true;
-        public string Name { get; set; } = "C";
+        public string Name { get; set; } = "Cmaj7";
         public string? Tone { get; set; }
         public string? Type { get; set; }
         public string? Extension { get; set; }
         public string? Inst { get; set; }
-        public bool Progression { get; set; }
+        public string? Progression { get; set; }
         public bool TriadOrSeventh { get; set; }
-
+        
     }
 }
